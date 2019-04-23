09a516405f5189a0d319fc65d3a87563fef2a12c
FoxyRare <46086428+FoxyRare@users.noreply.github.com>
2019-04-23 19:24:43 +0300

    Add files via upload

---
+++ game/ITSchoolMeaning.java
@@ -0,0 +1,28 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class ITSchoolMeaning extends Game {
+    SpriteBatch batch;
+    BitmapFont font1;
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font1 = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        batch.dispose();
+        font1.dispose();
+    }
+}
